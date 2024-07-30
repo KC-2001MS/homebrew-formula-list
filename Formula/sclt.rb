e47179bbe7eccee3532bd6eb7a8c7ae5e1990014
KC-2001MS <iroiro.work1234@gmail.com>
2024-07-31 03:07:59 +0900

    fix sclt.rb

--- Formula/sclt.rb
+++ Formula/sclt.rb
@@ -1,5 +1,5 @@
 # sclt.rb
-class sclt < Formula
+class Sclt < Formula
   desc "Sample CLI app for a library to build a CLI app display in Swift"
   homepage "https://github.com/KC-2001MS/SwiftLI"
   url "https://github.com/KC-2001MS/SwiftLI.git", tag: "0.0.2"
