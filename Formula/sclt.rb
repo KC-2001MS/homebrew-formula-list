225e02ad2321e3f2228f0fb4b366daa1bdfbc2c7
KC-2001MS <iroiro.work1234@gmail.com>
2024-07-31 03:00:13 +0900

    first commit

---
+++ Formula/sclt.rb
@@ -0,0 +1,17 @@
+// sclt.rb
+class sclt < Formula
+  desc "Sample CLI app for a library to build a CLI app display in Swift"
+  homepage "https://github.com/KC-2001MS/SwiftLI"
+  url "https://github.com/KC-2001MS/SwiftLI.git", tag: "0.0.2"
+  version "0.0.2"
+
+  depends_on "xcode": [:build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}sclt", "list"
+  end
+end
